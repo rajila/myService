511d2c58e1c96ebaa88e342595dde83c701642a2
Ronald <rdajila@gmail.com>
2019-06-02 02:42:51 +0200

    Agregar archivos de proyecto.

---
+++ ServiceOneWCF/IServiceOne.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Librerias
+using System.ServiceModel;
+
+namespace ServiceOneWCF
+{
+    [ServiceContract]
+    public interface IServiceOne
+    {
+        [OperationContract]
+        string getMessage(string name);
+    }
+}
---
+++ ServiceOneWCF/ServiceOne.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceOneWCF
+{
+    public class ServiceOne : IServiceOne
+    {
+        public string getMessage(string name)
+        {
+            return "Hola mundo " + name;
+        }
+    }
+}
